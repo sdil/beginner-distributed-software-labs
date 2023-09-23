27291b1e1eae20c87f23d72b9ebb561ad0fd389c
Mohamad Fadhil <piukul@yahoo.com.my>
2023-09-24 04:54:45 +0800

    Add kafka-kraft script

---
+++ kafka/setup-kraft.sh
@@ -0,0 +1,13 @@
+apt-get install openjdk-21-jdk wget
+wget https://dlcdn.apache.org/kafka/3.5.0/kafka_2.13-3.5.0.tgz
+
+tar -xzf kafka_2.13-3.5.0.tgz
+cd kafka_2.13-3.5.0
+
+KAFKA_CLUSTER_ID="$(bin/kafka-storage.sh random-uuid)"
+./bin/kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c config/kraft/server.properties
+
+# update node id
+# update server quorum
+
+./bin/kafka-server-start.sh config/kraft/server.properties
