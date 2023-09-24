720fad28eb56b3186d451d7976ec4cc6de423215
Mohamad Fadhil <piukul@yahoo.com.my>
2023-09-24 08:03:59 +0800

    Add postgresql logical replication

--- kafka/setup-kraft.sh
+++ kafka/setup-kraft.sh
@@ -10,4 +10,4 @@ KAFKA_CLUSTER_ID="$(bin/kafka-storage.sh random-uuid)"
 # update node id
 # update server quorum
 
-./bin/kafka-server-start.sh config/kraft/server.properties
+JMX_PORT=7001 ./bin/kafka-server-start.sh config/kraft/server.properties
